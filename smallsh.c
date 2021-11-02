4793f57bb5d28efc1c3c99f5d256fb637cf0da5e
c-a-ray <c.a.ray@outlook.com>
2021-11-02 15:44:36 -0700

    Add comments

--- smallsh.c
+++ smallsh.c
@@ -1,3 +1,16 @@
+/**
+ * @file smallsh.c
+ * @author Cody Ray <rayc2@oregonstate.edu>
+ * @version 1.0
+ * @section DESCRIPTION
+ *
+ * For OSU CS 344
+ * Assignment 3
+ * 
+ * Source file for smallsh, 
+ * a shell that implements a subset of features of well-known shells
+ */
+
 #include <signal.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -8,32 +21,49 @@
 
 #include "smallsh.h"
 
+// Declared as a global variable because it is needed in the SIGTSTP handler
+// but cannot be passed as a parameter to `void (*sa_handler)(int)`
 bool allow_bg = true;
+
+// void (*__sa_handler)(int)
 
 int main()
 {
+    // Setup SIGINT (Ctrl-C) and SIGTSTP (Ctrl-Z)
     struct sigaction sa_SIGINT = setup_sigactions();
 
+    // Create new struct to store command info
     struct command cmd;
-    cmd.args = malloc(sizeof(char *) * MAX_ARGS);
+    cmd.args = malloc(sizeof(char *) * MAX_ARGS + 1);
 
+    // Create new struct to store info about background processes
     struct background bg;
+
+    // To store exit status or terminating signal of the last fg process
     int lastExit = 0;
+
+    // To determine when to exit the shell
     bool cont = true;
 
     do
     {
+        // Display prompt
         printf(": ");
         fflush(stdout);
 
+        // Zero out members of the command struct so we can store the next command
         reset_command(&cmd);
 
+        // Read current command, parse it, and store it in cmd
         parse_command(read_command(), &cmd);
+
+        // Execute the current command
         cont = exec_cmd(&cmd, &lastExit, sa_SIGINT, &bg);
+
+        // Check on background processes
         bg = run_bg_census(bg, lastExit);
 
-        fflush(stdout);
-        free(cmd.args);
+        // Free memory allocated for input and output files
         free(cmd.inFile);
         free(cmd.outFile);
     } while (cont);
@@ -41,31 +71,38 @@ int main()
 
 struct sigaction setup_sigactions()
 {
+    // Code from "Exploration: Signal Handling API"
+
+    // Initialize sigaction struct for SIGTSTP and register handler to togger foreground-only mode
     struct sigaction sa_SIGTSTP = {0};
     sa_SIGTSTP.sa_handler = handle_SIGTSTP;
-    sigfillset(&sa_SIGTSTP.sa_mask);
+    sigfillset(&sa_SIGTSTP.sa_mask); //
     sa_SIGTSTP.sa_flags = 0;
     sigaction(SIGTSTP, &sa_SIGTSTP, NULL);
 
+    // Initialize sigaction struct for SIGINT and ignore signal
     struct sigaction sa_SIGINT = {0};
     sa_SIGINT.sa_handler = SIG_IGN;
     sigfillset(&sa_SIGINT.sa_mask);
     sa_SIGINT.sa_flags = 0;
     sigaction(SIGINT, &sa_SIGINT, NULL);
 
+    // Return SIGINT action for later use
     return sa_SIGINT;
 }
 
 void handle_SIGTSTP(int signo)
 {
-    if (allow_bg)
+    if (allow_bg) // Not currently in foreground-only mode
     {
+        // Switch to foreground-only mode
         allow_bg = false;
         const char *msg = "Entering foreground-only mode (& is now ignored)\n";
         write(STDOUT_FILENO, msg, strlen(msg));
     }
-    else
+    else // Currently in foreground-only mode
     {
+        // Exit foreground-only mode
         allow_bg = true;
         const char *msg = "Exiting foreground-only mode\n";
         write(STDOUT_FILENO, msg, strlen(msg));
@@ -76,9 +113,14 @@ void handle_SIGTSTP(int signo)
 
 void reset_command(struct command *cmd)
 {
-    memset((*cmd).args, 0, MAX_ARGS);
-    (*cmd).run_in_bg = false;
+    // Zero out args so we can fill it again
+    memset((*cmd).args, 0, MAX_ARGS + 1);
     (*cmd).nargs = 0;
+
+    // By default, don't run in background
+    (*cmd).run_in_bg = false;
+
+    // Don't carry over i/o redirects
     (*cmd).inFile = NULL;
     (*cmd).outFile = NULL;
 }
@@ -87,157 +129,175 @@ char *read_command(void)
 {
     size_t buffer_size = 0;
     char *line = NULL;
-    getline(&line, &buffer_size, stdin);
-    return line;
+    getline(&line, &buffer_size, stdin); // Read command
+    return line;                         // Return command string so it can be parsed
 }
 
 void parse_command(char *cmd_str, struct command *cmd)
 {
-    char **tokens = malloc(sizeof(char *) * MAX_INPUT_LEN);
+    // Allocate enough space for 512 arguments
+    char **tokens = malloc(sizeof(char *) * MAX_INPUT_LEN + 1);
+
+    // Get the first argument
     char *token = strtok(cmd_str, TOKEN_DELIMITER);
 
+    // Iterate through all arguments in command
     while (token != NULL)
     {
-        if (strmatch(token, "<"))
+        if (strmatch(token, "<")) // Input redirect encountered
         {
-            token = strtok(NULL, TOKEN_DELIMITER);
-            (*cmd).inFile = malloc(sizeof(char) * MAX_FILENAME_LEN);
-            strcpy((*cmd).inFile, token);
+            token = strtok(NULL, TOKEN_DELIMITER);                       // Move to the next argument
+            (*cmd).inFile = malloc(sizeof(char) * MAX_FILENAME_LEN + 1); // Allocate enough space for a file name
+            strcpy((*cmd).inFile, token);                                // Copy the token into the command struct
         }
-        else if (strmatch(token, ">"))
+        else if (strmatch(token, ">")) // Output redirect encountered
         {
-            token = strtok(NULL, TOKEN_DELIMITER);
-            (*cmd).outFile = malloc(sizeof(char) * MAX_FILENAME_LEN);
-            strcpy((*cmd).outFile, token);
+            token = strtok(NULL, TOKEN_DELIMITER);                        // Move to the next token
+            (*cmd).outFile = malloc(sizeof(char) * MAX_FILENAME_LEN + 1); // Allocate enough space for a file name
+            strcpy((*cmd).outFile, token);                                // Copy the token into the command struct
         }
-        else
+        else // Not a redirect character
         {
-            if (strmatch(token, "$$"))
+            if (strmatch(token, "$$")) // PID variable encountered
             {
-                char *pid_str = malloc(sizeof(char) * MAX_PID_LEN);
-                sprintf(pid_str, "%d", getpid());
-                tokens[(*cmd).nargs] = pid_str;
+                char *pid_str = malloc(sizeof(char) * MAX_PID_LEN + 1); // Create a string to hold the PID
+                sprintf(pid_str, "%d", getpid());                       // Write the PID to the PID string
+                tokens[(*cmd).nargs] = pid_str;                         // Replace $$ with the PID string
             }
-            else
-                tokens[(*cmd).nargs] = token;
+            else                              // It's a regular argument
+                tokens[(*cmd).nargs] = token; // Store it as it is
 
-            (*cmd).nargs++;
+            (*cmd).nargs++; // Increment the number of arguments
         }
 
-        token = strtok(NULL, TOKEN_DELIMITER);
+        token = strtok(NULL, TOKEN_DELIMITER); // Get the next token
     }
 
+    // Check if the last argument is the background character
     if ((*cmd).nargs > 1 && strmatch(tokens[(*cmd).nargs - 1], "&"))
     {
-        if (allow_bg)
-            (*cmd).run_in_bg = true;
+        if (allow_bg)                // Not in foreground-only mode
+            (*cmd).run_in_bg = true; // Indicate the command should run in the background
 
+        // Remove the character
         (*cmd).nargs--;
         tokens[(*cmd).nargs] = NULL;
     }
 
-    (*cmd).args = tokens;
+    (*cmd).args = tokens; // Store the arguments in the command struct
 }
 
 bool strmatch(const char *str1, const char *str2)
 {
     if (strcmp(str1, str2) == 0)
-        return true;
-    return false;
+        return true; // Strings match
+    return false;    // Strings don't match
 }
 
 bool exec_cmd(struct command *cmd, int *lastExit, struct sigaction sa_SIGINT, struct background *bg)
 {
+    // Pull out the first and second arguments to make syntax less busy
     char *firstArg = (*cmd).args[0];
     char *secondArg = (*cmd).args[1];
 
-    if (firstArg == NULL || (strncmp(firstArg, "#", 1)) == 0)
+    // Check if first command is a built-in function
+    if (firstArg == NULL || (strncmp(firstArg, "#", 1)) == 0) // For empty line or comment, do nothing
         return true;
-    else if (strmatch(firstArg, "exit"))
+    else if (strmatch(firstArg, "exit")) // For "exit", return false to exit the main loop
         return false;
-    else if (strmatch(firstArg, "cd"))
+    else if (strmatch(firstArg, "cd")) // For "cd", change the directory based on the second argument
         return smallsh_cd(secondArg);
-    else if (strmatch(firstArg, "status"))
+    else if (strmatch(firstArg, "status")) // For "status", print the exit/termination value of the last fg process
         return smallsh_status(*lastExit);
 
+    // Command is not built in, outsource execution
     run_non_builtin(cmd, lastExit, sa_SIGINT, bg);
     return true;
 }
 
 bool smallsh_cd(char *dir)
 {
-    if (dir == NULL)
+    if (dir == NULL) // "cd" is the only argument
     {
         const char *homeDir = getenv("HOME");
-        if (chdir(homeDir) == -1)
-            printf("directory %s not found\n", homeDir);
-        else
-            printf("changing to directory %s\n", homeDir);
+        if (chdir(homeDir) == -1)                                 // Change to HOME directory
+            fprintf(stderr, "directory %s not found\n", homeDir); // Directory wasn't found, so print an error
     }
-    else if (chdir(dir) == -1)
-        printf("directory %s not found\n", dir);
+    else if (chdir(dir) == -1)                   // A path was specified, change the working directory to that path
+        printf("directory %s not found\n", dir); // Directory wasn't found, so print an error
 
-    return true;
+    fflush(stderr);
+    return true; // Always continue shell execution after "cd"
 }
 
 bool smallsh_status(int lastExit)
 {
-    if (WIFEXITED(lastExit))
-        printf("exit value %d\n", WEXITSTATUS(lastExit));
-    else
-        printf("terminated by signal %d\n", WTERMSIG(lastExit));
-    return true;
+    if (WIFEXITED(lastExit))                                     // Last foreground process exited on its own
+        printf("exit value %d\n", WEXITSTATUS(lastExit));        // Print the exit value
+    else                                                         // The last foreground process was terminated
+        printf("terminated by signal %d\n", WTERMSIG(lastExit)); // Print the terminating signal
+    return true;                                                 // Always continue shell execution after "cd"
 }
 
 void run_non_builtin(struct command *cmd, int *lastExit, struct sigaction sa_SIGINT, struct background *bg)
 {
-    pid_t pid = fork();
-    if (pid < 0)
+    // A lot of this code comes from the Module 4 explorations
+
+    pid_t pid = fork(); // Create a new process
+    if (pid < 0)        // The process failed, print an error and exit
     {
         fprintf(stderr, "out of resources\n");
         exit(EXIT_FAILURE);
     }
-    else if (pid == 0)
+    else if (pid == 0) // Child process
     {
-        if ((*cmd).run_in_bg)
+        if ((*cmd).run_in_bg) // Process is running in the background
         {
             if ((*cmd).inFile == NULL)
-                handle_redirect("/dev/null", "in");
+                handle_redirect("/dev/null", "in"); // No input redirect, use /dev/null
             if ((*cmd).outFile == NULL)
-                handle_redirect("/dev/null", "out");
+                handle_redirect("/dev/null", "out"); // No output redirect, use /dev/null
         }
-        else
+        else // Process is not running in the background
         {
+            // Set the SIGINT action to its default behavior
             sa_SIGINT.sa_handler = SIG_DFL;
             sigaction(SIGINT, &sa_SIGINT, NULL);
-
-            if (((*cmd).inFile != NULL))
-                handle_redirect((*cmd).inFile, "in");
-            if (((*cmd).outFile != NULL))
-                handle_redirect((*cmd).outFile, "out");
         }
 
+        if (((*cmd).inFile != NULL))
+            handle_redirect((*cmd).inFile, "in"); // Command has an input redirect, so redirect to specified file
+        if (((*cmd).outFile != NULL))
+            handle_redirect((*cmd).outFile, "out"); // Command has an output redirect, so redirect to specified file
+
+        // Execute command
         if (execvp((*cmd).args[0], (*cmd).args))
         {
+            // An error occurred, print an error and exit
             fprintf(stderr, "%s: no such file or directory\n", (*cmd).args[0]);
             exit(EXIT_FAILURE);
         }
     }
-    else
+    else // Parent process
     {
-        if (!((*cmd).run_in_bg))
+        if (!((*cmd).run_in_bg)) // Child process not running in background, so wait until it completes
         {
+            // Wait until the process exits on its own or a terminating signal is detected
             do
             {
                 waitpid(pid, lastExit, 0);
             } while (!WIFEXITED(*lastExit) && !WIFSIGNALED(*lastExit));
 
+            // If the process was terminated by a signal, print a message about the signal
             if (WIFSIGNALED(*lastExit))
                 printf("terminated by signal %d\n", WTERMSIG(*lastExit));
         }
-        else
+        else // Child process running in the background, don't wait
         {
+            // Print a message about the background PID
             printf("background pid: %d\n", pid);
+
+            // Add the background process' PID to the background struct so we can keep an eye on them
             (*bg).pids[(*bg).size++] = pid;
         }
     }
@@ -245,40 +305,47 @@ void run_non_builtin(struct command *cmd, int *lastExit, struct sigaction sa_SIG
 
 void handle_redirect(char *file, const char *direction)
 {
-    int fd, dup_res;
+    // This code takes from "Exploration: Processes and I/O"
 
-    if (strmatch(direction, "in"))
+    int fd;      // File descriptor
+    int dup_res; // Dup result
+
+    if (strmatch(direction, "in")) // Input redirect
     {
-        fd = open(file, O_RDONLY);
+        fd = open(file, O_RDONLY); // Open the file only for reading
         if (fd == -1)
         {
+            // Error opening file; print error and exit
             fprintf(stderr, "cannot open file %s for input\n", file);
             exit(EXIT_FAILURE);
         }
-        dup_res = dup2(fd, 0);
+        dup_res = dup2(fd, 0); // Redirect to stdin
         if (dup_res == -1)
         {
+            // Error using dup2; print error and exit
             fprintf(stderr, "cannot redirect input to file %s\n", file);
             exit(EXIT_FAILURE);
         }
-        close(fd);
     }
-    else
+    else // Output redirect
     {
+        // Open the file only for writing; create if doesn't exit; truncate if it does
         fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
         if (fd == -1)
         {
+            // Error opening file; print error and exit
             fprintf(stderr, "cannot open file %s for output\n", file);
             exit(EXIT_FAILURE);
         }
         dup_res = dup2(fd, 1);
         if (dup_res == -1)
         {
+            // Error using dup2; print error and exit
             fprintf(stderr, "cannot redirect output to file %s\n", file);
             exit(EXIT_FAILURE);
         }
-        close(fd);
     }
+    close(fd); // Close the file
 }
 
 struct background run_bg_census(struct background bg, int lastExit)
@@ -296,5 +363,6 @@ struct background run_bg_census(struct background bg, int lastExit)
         else if (pid == 0)
             still_running.pids[still_running.size++] = bg.pids[i];
     }
+    fflush(stdout);
     return still_running;
 }
--- smallsh.h
+++ smallsh.h
@@ -1,39 +1,172 @@
+/**
+ * @file smallsh.h
+ * @author Cody Ray <rayc2@oregonstate.edu>
+ * @version 1.0
+ * @section DESCRIPTION
+ *
+ * For OSU CS 344
+ * Assignment 3
+ * 
+ * Header file for smallsh, 
+ * a shell that implements a subset of features of well-known shells
+ */
+
 #ifndef SMALLSH
 #define SMALLSH
 
-#define MAX_INPUT_LEN 2048
-#define MAX_FILENAME_LEN 256
-#define MAX_ARGS 512
-#define BG_CAP 1000
-#define MAX_PID_LEN 8
-#define TOKEN_DELIMITER " \r\a\n\t"
+#define MAX_INPUT_LEN 2048          // Per assignment, max length of a command line
+#define MAX_ARGS 512                // Per assignment, max number of arguments
+#define MAX_FILENAME_LEN 256        // Per OS spec, max length of a file name
+#define MAX_PID_LEN 7               // Per OS spec, max number of digits in a PID
+#define BG_CAP 1000                 // Max number of background processes to allow
+#define TOKEN_DELIMITER " \r\a\n\t" // Delimiters for splitting command line into args
 
+/** 
+ * Stores information about a command entered into smallsh
+ */
 struct command
 {
-    char **args;
-    int nargs;
-    char *inFile;
-    char *outFile;
-    bool run_in_bg;
+    char **args;    //< Individual arguments of the command
+    int nargs;      //< Number of arguments in the command
+    char *inFile;   //< Input redirect file specified in command
+    char *outFile;  //< Output redirect file specified in command
+    bool run_in_bg; //< Determine if command should run as a background process
 };
 
+/** 
+ * Stores information about currently running background processes
+ */
 struct background
 {
-    pid_t pids[BG_CAP];
-    int size;
+    pid_t pids[BG_CAP]; //< Array to hold PIDs of all currently running background processes
+    int size;           //< The number of currently running background processes
 };
 
+/**
+ * Sets up handling of SIGTSTP (Ctrl-Z) and SIGINT (Ctrl-C)
+ * 
+ * Catch SIGTSTP and ignore SIGINT, but return SIGINT sigaction struct for later use
+ * 
+ * @return SIGINT sigaction struct
+ */
 struct sigaction setup_sigactions();
+
+/**
+ * sa_handler ((*sa_handler)(int)) for SIGTSTP
+ * 
+ * When SIGTSTP occurs, toggles foreground-only mode.
+ * Uses global bool variable allow_bg to accomplish this.
+ * 
+ * @param signo required but not used
+ */
 void handle_SIGTSTP(int);
+
+/**
+ * Reset members of the command structure so it can be reused/filled again
+ * 
+ * @param cmd pointer to command struct to reset
+ */
 void reset_command(struct command *);
+
+/**
+ * Get the last command entered by the user
+ * 
+ * @return the user-entered command
+ */
 char *read_command(void);
+
+/**
+ * Parse a command line
+ * 
+ * Split the command line into tokens, count the arguments, detect any i/o redirects, 
+ * expand the PID variable, and check for the background commands, filling 
+ * a command struct with the information
+ * 
+ * @param cmd_str the full command line
+ * @param cmd the command struct to store parsed data in
+ */
 void parse_command(char *, struct command *);
+
+/**
+ * Determine whether two strings are equal
+ * 
+ * This is just to make the syntax for checking string equality a little nicer
+ * 
+ * @param str1 the first string to compare
+ * @param str2 the second string to compare
+ * @return true if str1 == str2, false otherwise
+ */
 bool strmatch(const char *, const char *);
+
+/**
+ * Execute the command entered by the user
+ * 
+ * Execute built-in function if one exists, otherwise outsource execution, and
+ * tell the main loop whether to continue or not. If the command is an empty line
+ * or a comment, then continue without doing anything. If the command is "exit",
+ * exit the main loop. Otherwise execute the appropriate built-in or outsource,
+ * then continue.
+ * 
+ * @param  cmd the command struct holding information about the current command
+ * @param  lastExit the exit value or terminating signal of the last process
+ * @param  sa_SIGINT the SIGINT action struct to be passed down to run_non_builtin()
+ * 
+ * @return true if shell should continue running, false if the shell should stop running
+ */
 bool exec_cmd(struct command *, int *, struct sigaction, struct background *);
+
+/**
+ * Change the current directory
+ * 
+ * @param  dir is the path (relative or absolute) to the directory to change to
+ * 
+ * @return always true
+ */
 bool smallsh_cd(char *);
+
+/**
+ * Change the current directory
+ * 
+ * @param  dir is the path (relative or absolute) to the directory to change to
+ * 
+ * @return always true
+ */
 bool smallsh_status(int);
+
+/**
+ * Execute a non-built-in command
+ * 
+ * Create a new process and execute a command in the foreground or background.
+ * If running in background and no i/o redirectoins are specified, redirect
+ * input and output both to /dev/null. If not running in background, set the
+ * SIGINT action to its default and handle any i/o redirects. 
+ * 
+ * @param cmd the command struct holding information about the command to execute
+ * @param lastExit the exit value or terminating signal of the last process
+ * @param sa_SIGINT the SIGINT action struct to be passed down to run_non_builtin()
+ * @param bg the background struct holding information about the currently running background processes
+ */
 void run_non_builtin(struct command *, int *, struct sigaction, struct background *);
+
+/**
+ * Handle input and output file redirects
+ * 
+ * If direction is "in", redirect stdin to the specified file,
+ * otherwise redirect stdout to the specified file
+ * 
+ * @param file the path to the file to redirect to
+ * @param direction a string indicating whether the redirect is for input or output
+ */
 void handle_redirect(char *, const char *);
+
+/**
+ * Check on and clean up background processes
+ * 
+ * @param  bg the background struct holding information about the processes currently running in the background
+ * @param  lastExit the exit value or terminating signal of the last process
+ * 
+ * @return a new background struct with information about the still-running background processess
+ */
 struct background run_bg_census(struct background, int);
 
 #endif
